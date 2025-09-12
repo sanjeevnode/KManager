7fb66fd4dbfe6c63a550859b145c3a32cc9868de
sanjeevnode <me.sanjeevks@gmail.com>
2025-09-12 14:56:49 +0530

    feat: add gradient text utility and enhance LoginCard and Navbar components; remove unused navitems

--- components/LoginCard.tsx
+++ components/LoginCard.tsx
@@ -4,28 +4,26 @@ import { signIn } from "next-auth/react"
 import { Card, CardContent } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import { FcGoogle } from "react-icons/fc"
-import { Section } from "lucide-react"
 
 export default function LoginCard() {
     return (
         <div className="flex h-full items-center justify-center ">
-            <Card className="w-full max-w-sm rounded shadow">
+            <Card className="w-full max-w-sm rounded shadow-none">
                 <CardContent className="p-6 flex flex-col items-center space-y-6">
-                   <div className="flex items-center">
-                    <div className="bg-gradient-to-r from-orange-600 to-amber-600 p-1 rounded">
-                                            <Section className="w-6 h-6 text-white" />
-                                        </div>
-                                        <span className="ml-3 text-xl font-bold font-poppins gradient-text">KManager</span>
-                   </div>
-                    <p className="text-gray-500 text-center">
-                        Log in to your account
-                    </p>
+                    <div className="flex flex-col space-y-2">
+                        <p className="text-2xl md:text-3xl font-semibold text-black">
+                            Welcome to <span className="gradient-text">KManager</span>
+                        </p>
+                        <p className="text-gray-800">
+                            Manage your secrets efficiently and effectively.
+                        </p>  
+                        </div>                
                     <Button
                         onClick={() => signIn("google")}
                         variant="outline"
                         className="w-full flex items-center justify-center space-x-3 rounded cursor-pointer"
                     >
-                        <FcGoogle/>
+                        <FcGoogle />
                         <span>Continue with Google</span>
                     </Button>
                 </CardContent>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,17 @@
 "use client"
 
-import { LogOut, Section } from "lucide-react"
+import { LogOut, Menu, Section } from "lucide-react"
 import Link from "next/link"
 import { signOut, useSession } from "next-auth/react"
 import Image from "next/image"
-import { useCallback } from "react"
+import { useCallback, useState } from "react"
 import navigationStore from "@/store/navigationStore"
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "./ui/sheet"
 
 export default function Navbar() {
     const { data: session, status } = useSession()
     const navIndex = navigationStore((state) => state.navIndex);
-    
+
     const handleSignOut = useCallback(() => {
         signOut()
     }, [])
@@ -18,50 +19,107 @@ export default function Navbar() {
     const isAuthenticated = status === "authenticated"
     const userImage = session?.user?.image
 
-
+    if (!isAuthenticated) {
+        return <>   </>
+    }
     return (
         <nav className="fixed top-0 w-full h-16 bg-white z-50">
-            <div className="mx-auto max-w-7xl flex items-center justify-between py-2 h-full md:px-6 px-4 border-b">
+            <div className="mx-auto max-w-7xl flex items-center justify-between py-2 h-full md:px-6 px-4 border-b rounded">
                 {/* Logo section */}
                 <Link href="/" className="flex items-center hover:opacity-90 transition-opacity">
                     <div className="bg-gradient-to-r from-orange-600 to-amber-600 p-1 rounded">
                         <Section className="w-6 h-6 text-white" />
                     </div>
-                    <span className="ml-3 text-xl font-bold font-poppins gradient-text">KManager</span>
+                    <span className="ml-3 text-xl font-bold">KManager</span>
                 </Link>
-                
+
                 {/* User section */}
-                {
-                    isAuthenticated &&(
-                        <div className="space-x-6 hidden sm:flex items-center ">
-                            <span className={`text-gray-600 font-medium ${navIndex === 0 ? 'font-bold text-orange-800' : ''} cursor-pointer`}
-                            onClick={() => navigationStore.getState().setNavIndex(0)}
-                            >
-                                My Secrets
-                            </span>
-                            <span className={`text-gray-600 font-medium ${navIndex === 1 ? 'font-bold text-orange-800' : ''} cursor-pointer`}
-                            onClick={() => navigationStore.getState().setNavIndex(1)}
-                            >
-                                Profile
-                            </span>
-                            {userImage && (
-                                <Image
-                                    src={userImage}
-                                    alt="User Avatar"
-                                    width={32}
-                                    height={32}
-                                    className="rounded-full object-cover outline"
-                                    priority={false}
-                                />
-                            )}
 
-                            <LogOut className="w-5 h-5 cursor-pointer text-gray-600 hover:text-gray-800" onClick={handleSignOut} />
+                <div className="space-x-6 hidden sm:flex items-center ">
+                    <span className={`text-gray-600 font-medium ${navIndex === 0 ? 'font-bold text-orange-800' : ''} cursor-pointer`}
+                        onClick={() => navigationStore.getState().setNavIndex(0)}
+                    >
+                        My Secrets
+                    </span>
+                    <span className={`text-gray-600 font-medium ${navIndex === 1 ? 'font-bold text-orange-800' : ''} cursor-pointer`}
+                        onClick={() => navigationStore.getState().setNavIndex(1)}
+                    >
+                        Profile
+                    </span>
+                    {userImage && (
+                        <Image
+                            src={userImage}
+                            alt="User Avatar"
+                            width={32}
+                            height={32}
+                            className="rounded-full object-cover outline"
+                            priority={false}
+                        />
+                    )}
 
-                        </div>
-                    )
-                }
-                
+                    <LogOut className="w-5 h-5 cursor-pointer text-gray-600 hover:text-gray-800" onClick={handleSignOut} />
+
+                </div>
+                <MobileNavbar />
             </div>
         </nav>
+    )
+}
+
+
+
+const MobileNavbar = () => {
+    const [open, setOpen] = useState(false);
+
+    const handleItemClick = (action?: () => void) => {
+        if (action) action();
+        setOpen(false);
+    };
+    const navIndex = navigationStore((state) => state.navIndex);
+
+    const handleSignOut = useCallback(() => {
+        signOut()
+    }, [])
+    return (
+        <div className="sm:hidden">
+            <Sheet open={open} onOpenChange={setOpen}>
+                <SheetTrigger>
+                    <Menu className="cursor-pointer" />
+                </SheetTrigger>
+                <SheetContent className="w-full">
+                    <SheetHeader>
+                        <SheetTitle>
+                            <Link href="/" className="flex items-center hover:opacity-90 transition-opacity">
+                                <div className="bg-gradient-to-r from-orange-600 to-amber-600 p-1 rounded">
+                                    <Section className="w-6 h-6 text-white" />
+                                </div>
+                                <span className="ml-3 text-xl font-bold font-poppins gradient-text">KManager</span>
+                            </Link>
+                        </SheetTitle>
+                    </SheetHeader>
+                    <div className="flex flex-col space-y-2 items-center">
+                        <span className={`text-gray-600 font-medium ${navIndex === 0 ? 'font-bold text-orange-800' : ''} cursor-pointer w-full p-4 border-b `}
+                            onClick={() => handleItemClick(() => navigationStore.getState().setNavIndex(0))}
+                        >
+                            My Secrets
+                        </span>
+                        <span className={`text-gray-600 font-medium ${navIndex === 1 ? 'font-bold text-orange-800' : ''} cursor-pointer w-full p-4 border-b`}
+                            onClick={() => handleItemClick(() => navigationStore.getState().setNavIndex(1))}
+                        >
+                            Profile
+                        </span>
+
+
+                        <span
+                            className="text-gray-600 font-medium cursor-pointer w-full p-4 border-b"
+                            onClick={() => handleItemClick(handleSignOut)}
+                        >
+                            Logout
+                        </span>
+
+                    </div>
+                </SheetContent>
+            </Sheet>
+        </div>
     )
 }
